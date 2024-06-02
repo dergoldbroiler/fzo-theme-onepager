f78c63325931f929e6d4b84e1fc3d5c290adfb65
Bjorn Zschernack <dergoldbroiler@gmail.com>
2024-06-02 11:38:51 +0200

    update header

--- front-page.php
+++ front-page.php
@@ -2,9 +2,9 @@
 
 
 
-<div class="container p-0" id="main">
+<div class="container-fluid p-0" id="main">
 
-    <div class="row">
+    <div class="row bg-lightgreen">
         <div class="col-12">
             <?php get_template_part('template-parts/posts/postgrid'); ?>
         </div>
