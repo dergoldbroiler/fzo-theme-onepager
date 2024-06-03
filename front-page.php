be6d35ff97716006296591a3054136c23257bc04
Bjorn Zschernack <dergoldbroiler@gmail.com>
2024-06-03 18:03:35 +0200

    subpages sc

--- front-page.php
+++ front-page.php
@@ -4,12 +4,12 @@
 
 <div class="container-fluid p-0" id="main">
 
-    <div class="row bg-lightgreen">
+    <div class="row bg-lightgreen pb-5">
         <div class="col-12">
             <h1 class="text-center m-0 mt-5 m-lg-5 p-5 fs36" id="ceramic-experts-top">Ceramic Experts bietet Produkte und insbesondere <br>deren Dienstleistungen für hochklassige, langhaltende <br>und beständige Keramikbeschichtungen.</h1> 
         </div>
-        <div class="col-10 col-xxl-8 offset-1 offset-xxl-2">
-            <div class="container">
+        <div class="col-12 pb-5">
+            <div class="container contentarea">
 
                     <?php echo do_shortcode('[subpages id="leistungen"]'); ?>
             </div>   
