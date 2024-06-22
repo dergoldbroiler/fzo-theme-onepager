b9603679ed766141e28e7681b2f240350a855427
Bjorn Zschernack <dergoldbroiler@gmail.com>
2024-06-22 11:00:03 +0200

    update

--- template-parts/pages/subpage.php
+++ template-parts/pages/subpage.php
@@ -11,7 +11,13 @@
 
     <div class="single-post-content subpage-content d-flex flex-column p-5">
         <h2 class="fs23 mb-3"><?php echo $args->post_title; ?></h2>
-        <p class="teaser mb-4"><?php echo get_post_meta($args->ID,'teaser',true); ?></p>
+        <?php if(get_post_meta($args->ID,'teaser',true)): ?>
+            <p class="teaser mb-4"><?php echo get_post_meta($args->ID,'teaser',true); ?></p>
+        <?php endif; ?>    
+
+        <?php if(get_post_meta($args->ID,'fullteaser',true)): ?><br>
+            <?php echo apply_filters('the_content',get_post_meta($args->ID,'fullteaser',true)); ?>
+        <?php endif; ?>   
         <a href="<?php echo get_permalink($args->ID); ?>" class="rotate-90"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/icons/arrow_yellow.svg"></a>
     </div>
 </div>
